ee9a56fee7645c4e16b1c593080ecb72f6822b0a
Dipta <roydipta@iastate.edu>
2022-09-07 16:33:50 -0500

    Files Upload

---
+++ Data Collection/data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Nm=2   # Number of measurement per website
+Ns=1000  # Number of samples
+
+for ((i=1; i<=$Nm; i++))
+do
+  taskset -c 1 google-chrome www.twitter.com/ &
+  for ((j=1; j<=$Ns; j++))
+  do 
+    Freq_CPU=$(cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq)
+    echo $Freq_CPU
+    sleep 0.01s;
+  done
+  wmctrl -c "Google Chrome" &
+  sleep 10s;
+done
+
