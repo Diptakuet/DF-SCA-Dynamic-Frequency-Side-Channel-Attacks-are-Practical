7b0e5ee796c076bbbbad3a1434c8f33d4ef22366
Debopriya Roy Dipta <Diptakuet@users.noreply.github.com>
2022-09-13 21:36:28 -0500

    Files upload

---
+++ AMD Ryzen 5/Tor/confidance_score.m
@@ -0,0 +1,39 @@
+clc;clear all;
+cd 'D:\CPR Research\Topic1. CPU Frequency\Revision\Laptop\AMD\Tor\FinalData';
+a=readmatrix('Raw_prediction_cnn.txt');
+
+num_web=100;
+num_measure=1000;
+col_name={};
+for k=1:num_web
+    col_name=[col_name,num2str(k)];
+end
+row_name={};
+for k=1:num_measure
+    row_name=[row_name,num2str(k)];
+end
+
+a_new=array2table(a,'RowNames',row_name,'VariableNames',col_name);
+b=rows2vars(a_new);
+
+for i=1:num_measure
+    [sorted,index]=sortrows(b,strcat('x',num2str(i)),'descend');
+    web_index(:,i)=index(1:5);
+end
+
+p=1;
+c=1;
+tp=0;
+interval=10;
+for i=1:interval:num_measure
+    for j=i:i+interval-1
+        for k=1:5
+            if web_index(k,j)==c
+                tp=tp+1;
+            end
+        end
+    end
+    c=c+1;
+end
+
+score=(tp/1000)*100
